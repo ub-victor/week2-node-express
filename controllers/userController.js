1e041d8c937cc99b4fc2ad87e7f86e135c37d055
ub-victor <victoireushindi371@gmail.com>
2026-05-25 12:53:06 +0200

    Initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ const createUser = (req, res, next)=>{
         const {name, email} = req.body;
         // Validation
         if(!name || !email ){
-            
+            const error = new Error("Name and email are required")
         }
 
     }catch(error){
