9b575debc38da9dbd033e4b00f1e0b2c9370b8d3
ub-victor <victoireushindi371@gmail.com>
2026-05-25 17:39:29 +0200

    Initial commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 const userRoutes = require("./routes/userRouter");
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
 dotenv.config();
 
 const app = express();
