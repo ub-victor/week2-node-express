6e399376fec3c22ec425ad6f719a913d937c5870
ub-victor <victoireushindi371@gmail.com>
2026-05-25 14:44:23 +0200

    Initial commit

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,3 +1,8 @@
 const logger = (req, res, next)=>{
     const time = new Date().toISOString();
-}
+
+    console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+    next();
+};
+
+module.exports = logger;
